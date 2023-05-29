d90048e23aef9448c4bc157420309d69751d5075
warckut <warckut@gmail.com>
2023-05-29 23:15:02 +0500

    added throwing an error inside the parser module

--- src/parser.js
+++ src/parser.js
@@ -1,17 +1,24 @@
-export default ({ contents }) => {
-  const parser = new DOMParser();
-  const RSSDocument = parser.parseFromString(contents, 'text/xml');
+export default (xml) => {
+  try {
+    const parser = new DOMParser();
+    const RSSDocument = parser.parseFromString(xml, 'text/xml');
 
-  return {
-    feed: {
-      title: RSSDocument.querySelector('title').textContent,
-      description: RSSDocument.querySelector('description').textContent,
-    },
-    posts: Array.from(RSSDocument.querySelectorAll('item'))
-      .map((el) => ({
-        title: el.querySelector('title').textContent,
-        link: el.querySelector('link').textContent,
-        description: el.querySelector('description').textContent,
-      })),
-  };
+    return {
+      feed: {
+        title: RSSDocument.querySelector('title').textContent,
+        description: RSSDocument.querySelector('description').textContent,
+      },
+      posts: Array.from(RSSDocument.querySelectorAll('item'))
+        .map((el) => ({
+          title: el.querySelector('title').textContent,
+          link: el.querySelector('link').textContent,
+          description: el.querySelector('description').textContent,
+        })),
+    };
+  } catch (e) {
+    const error = new Error();
+    error.name = 'ParserError';
+    error.message = 'invalidRSS';
+    throw error;
+  }
 };
