7e199ee7036413f47207b8477120ec7f8ccbfd64
warckut <warckut@gmail.com>
2023-06-03 15:13:03 +0500

    changed error throwing

--- src/parser.js
+++ src/parser.js
@@ -1,24 +1,26 @@
 export default (xml) => {
-  try {
-    const parser = new DOMParser();
-    const RSSDocument = parser.parseFromString(xml, 'text/xml');
+  const parser = new DOMParser();
+  const RSSDocument = parser.parseFromString(xml, 'text/xml');
 
-    return {
-      feed: {
-        title: RSSDocument.querySelector('title').textContent,
-        description: RSSDocument.querySelector('description').textContent,
-      },
-      posts: Array.from(RSSDocument.querySelectorAll('item'))
-        .map((el) => ({
-          title: el.querySelector('title').textContent,
-          link: el.querySelector('link').textContent,
-          description: el.querySelector('description').textContent,
-        })),
-    };
-  } catch (e) {
+  const errorNode = RSSDocument.querySelector('parsererror');
+
+  if (errorNode) {
     const error = new Error();
     error.name = 'ParserError';
     error.message = 'invalidRSS';
     throw error;
   }
+
+  return {
+    feed: {
+      title: RSSDocument.querySelector('title').textContent,
+      description: RSSDocument.querySelector('description').textContent,
+    },
+    posts: Array.from(RSSDocument.querySelectorAll('item'))
+      .map((el) => ({
+        title: el.querySelector('title').textContent,
+        link: el.querySelector('link').textContent,
+        description: el.querySelector('description').textContent,
+      })),
+  };
 };
